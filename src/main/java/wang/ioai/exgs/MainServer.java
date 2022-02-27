0f7ab8dd3ff5b5e90cf3a28bef905f1b3b869f35
cwj5012 <cwj5012@gmail.com>
2022-02-27 16:41:21 +0800

    style

--- src/main/java/wang/ioai/exgs/MainServer.java
+++ src/main/java/wang/ioai/exgs/MainServer.java
@@ -8,11 +8,12 @@ import java.nio.file.Paths;
 
 public class MainServer {
     private static final Logger logger = LoggerFactory.getLogger(MainServer.class);
+    private static final String userDir = System.getProperty("user.dir");
 
     public static void main(String[] args) throws Exception {
-        logger.info("current dir: {}", System.getProperty("user.dir"));
+        logger.info("user.dir: {}", userDir);
         GData.init();
-        GData.config.load(Paths.get(System.getProperty("user.dir"), "config/server.json").toString());
+        GData.config.load(Paths.get(userDir, "config/server.json").toString());
         GData.tick.init();
         GData.tick.run();
         GData.netServer.init();
