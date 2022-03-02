9c161af83f85d6a6ab8451aef66c994039d20f99
cwj5012 <cwj5012@gmail.com>
2022-03-02 23:30:08 +0800

    boot add run

--- src/main/java/wang/ioai/exgs/MainServer.java
+++ src/main/java/wang/ioai/exgs/MainServer.java
@@ -1,18 +1,11 @@
 package wang.ioai.exgs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import wang.ioai.exgs.boot.Boot;
-import wang.ioai.exgs.data.GData;
-
-import java.nio.file.Paths;
 
 public class MainServer {
-
-
     public static void main(String[] args) throws Exception {
         var boot = new Boot();
         boot.init();
-
+        boot.run();
     }
 }
