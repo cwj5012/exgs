0923472b4c985c333129803c0d97f81a639897f8
cwj5012 <cwj5012@gmail.com>
2022-04-07 15:49:21 +0800

    1

--- src/main/java/wang/ioai/exgs/game/debug/HeartBeat.java
+++ src/main/java/wang/ioai/exgs/game/debug/HeartBeat.java
@@ -8,7 +8,7 @@ import wang.ioai.exgs.game.INetMessage;
 import wang.ioai.exgs.game.Player;
 import wang.ioai.exgs.core.net.msg.NetMessage;
 import wang.ioai.exgs.core.net.msg.ProtoMessage;
-import wang.ioai.exgs.pb.ProtoDebug;
+import wang.ioai.exgs.common.pb.ProtoDebug;
 
 public class HeartBeat implements INetMessage {
     private static final Logger logger = LoggerFactory.getLogger(HeartBeat.class);
