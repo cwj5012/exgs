739b2b75d0cb48b384635d9e64f888cd9161c180
cwj5012 <cwj5012@gmail.com>
2022-04-20 22:15:13 +0800

    1

---
+++ src/main/java/wang/ioai/exgs/exec/game/ModuleHandle.java
@@ -0,0 +1,5 @@
+package wang.ioai.exgs.exec.game;
+
+public interface ModuleHandle {
+    void init();
+}
--- src/main/java/wang/ioai/exgs/game/ModuleHandle.java
+++
@@ -1,5 +0,0 @@
-package wang.ioai.exgs.game;
-
-public interface ModuleHandle {
-    void init();
-}
